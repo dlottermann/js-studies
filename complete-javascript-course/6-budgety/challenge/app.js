0aef55b350cf24f74fd9a0c68114173ce12a5587
Diego Lottermann <diegolottermann@gmail.com>
2018-10-24 22:43:16 -0300

    Adjustement lecture 81

--- complete-javascript-course/6-budgety/challenge/app.js
+++ complete-javascript-course/6-budgety/challenge/app.js
@@ -1,65 +1,92 @@
-var BudgetController = (function(){
+var BudgetController = (function () {
 
-    return{
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
-    }
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
 
 })();
 
 
-var UIcontroller = (function(){
+var UIcontroller = (function () {
 
     var DOMstrings = {
-        inputType :'.add__type',
-        inputDescription :'.add__description',
-        inputValue :'.add__value',
-        inputBtnAdd :'.add__btn'
-    }
-    
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtnAdd: '.add__btn'
+    };
+
 
     return {
 
-        getInput:function(){
+        getInput: function () {
             return {
-                type : document.querySelector(DOMstrings.inputType).value,
-                description : document.querySelector(DOMstrings.inputDescription).value,
-                value : document.querySelector(DOMstrings.inputValue).value
-            }
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
         },
 
-        getDOMstrings : function(){
+        getDOMstrings: function () {
             return DOMstrings;
         }
-
-    }
-
-
-})();
-
-
-var controller =(function(ctrlBudget,ctrlUI){
-
-    var setupEventListeners =function(){
-
-        var DOM = ctrlUI.getDOMstrings(); 
-        //Click button
-          document.querySelector(DOM.inputBtnAdd).addEventListener('click',ctrlAddItem);
-            
-          document.addEventListener('keypress',function(event){
-                if(event.keyCode ===13 || event.which === 13){
-                    ctrlAddItem();
-                }
-            });
-        };
-
-    var ctrlAddItem =function(){
-            
-        var input = ctrlUI.getInput();
-        console.log(input);
 
     };
 
 
- 
+})();
 
-})(BudgetController,UIcontroller);
+
+var controller = (function (ctrlBudget, ctrlUI) {
+
+    var setupEventListeners = function () {
+
+        var DOM = ctrlUI.getDOMstrings();
+        //Click button
+        document.querySelector(DOM.inputBtnAdd).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+    var ctrlAddItem = function () {
+        var input = ctrlUI.getInput();
+        console.log(input);
+    };
+
+
+    return {
+        init: function () {
+            console.log("inicializado");
+            setupEventListeners();
+        }
+    };
+
+
+
+})(BudgetController, UIcontroller);
+
+controller.init();
