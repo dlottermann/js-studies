2e74e268770dca254b0203a0ea8d5a72dd9a3088
dlottermann <diegolottermann@gmail.com>
2018-10-30 13:10:08 -0300

    Ajustes Modulo

--- complete-javascript-course/6-budgety/challenge/app.js
+++ complete-javascript-course/6-budgety/challenge/app.js
@@ -22,7 +22,24 @@ var BudgetController = (function () {
             inc: 0
         }
     };
-
+    
+        return {
+            addItem:function(type,des,val){
+                var newItem,ID;
+                
+                ID=0;
+                if(type==='exp'){
+                   newItem = new Expense(ID,des,val);
+                    
+                }else if(type==='inc'){
+                    newItem = new Income(ID,des,val);   
+                }
+                
+                data.allItems[type].push(newItem);
+                return newItem;
+            }
+        }
+    
 
 })();
 
